14845e4502bbd7f42f8892619e09c489cb2a4c81
KitchenTile <azuldebenedetti@gmail.com>
2025-09-24 08:38:58 +0100

    Update orderControllers.ts

--- src/controllers/orderControllers.ts
+++ src/controllers/orderControllers.ts
@@ -5,8 +5,24 @@ import { connectDB } from "../config/db";
 export const addOrder = async (req: Request, res: Response) => {
   try {
     const db = await connectDB();
-    const allLessons = db.collection("lessons").find({});
+
+    const { lessonIds, name, phoneNumber, numOfSpaces } = req.body;
+    const lessonsOrdered = [];
+
+    lessonIds.forEach((lesson: string, index: number) => {
+      lessonsOrdered.push({
+        lessonId: lesson,
+        numOfSpaces: numOfSpaces[index],
+      });
+    });
+
+    const order = { name, phoneNumber, lessonsOrdered };
+
+    const addOrder = await db.collection("orders").insertOne(order);
+
+    return res.status(201).json({ order });
   } catch (error) {
     console.log(error);
+    return res.status(500).send("Server error");
   }
 };
