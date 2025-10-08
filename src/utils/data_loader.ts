06d7478f0c6570246daf286011b7119e88c33d02
Blue <azuldebenedetti@gmail.com>
2025-10-08 12:01:25 +0100

    Update data_loader.ts

--- src/utils/data_loader.ts
+++ src/utils/data_loader.ts
@@ -7,17 +7,18 @@ const generateEmbedding = await pipeline(
 );
 
 // first step of the injection pipeline - loading, chunking and embedding
-export const data_loader = async (file_dir: string) => {
+export const data_loader = async (file_dir: string): Promise<number[][]> => {
   try {
     const data = await fs.promises.readFile(file_dir, { encoding: "utf-8" });
     const chunks = data.split("\n\n");
 
     const embeddings = await Promise.all(
       chunks.map(async (chunk) => {
-        return generateEmbedding(chunk, {
+        const tensor = await generateEmbedding(chunk, {
           pooling: "mean",
           normalize: true,
         });
+        return tensor.tolist();
       })
     );
     return embeddings;
