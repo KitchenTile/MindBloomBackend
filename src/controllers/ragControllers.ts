6e31742fb6d99d4a3e535a04c3d4b9259c9e5b80
Blue <azuldebenedetti@gmail.com>
2025-10-09 22:22:23 +0100

    Create ragControllers.ts

---
+++ src/controllers/ragControllers.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { handleUserQuery } from "../utils/userDataRetrival.js";
+import OpenAI from "openai";
+
+export const handleChat = async (req: Request, res: Response) => {
+  try {
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+    const userQuery = req.body.userQuery;
+
+    console.log(userQuery);
+
+    const relataedChunks = await handleUserQuery(userQuery);
+
+    const chunkContent = relataedChunks.map((chunk) => chunk.chunk_content);
+
+    const gptReply = await client.chat.completions.create({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "system",
+          content: `Please only use the following information: ${chunkContent}.`,
+        },
+        {
+          role: "user",
+          content: `${userQuery}`,
+        },
+      ],
+    });
+
+    console.log(gptReply.choices[0].message);
+
+    return res.status(201).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+};
