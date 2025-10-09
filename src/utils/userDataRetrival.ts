4a6cab085e443086e38fd5a5b978ecae28886787
Blue <azuldebenedetti@gmail.com>
2025-10-09 22:22:37 +0100

    Update userDataRetrival.ts

--- src/utils/userDataRetrival.ts
+++ src/utils/userDataRetrival.ts
@@ -21,7 +21,9 @@ export const handleUserQuery = async (question: string) => {
 };
 
 //vectorize user's question
-const getQuestionEmbedding = async (question: string): Promise<number[]> => {
+export const getQuestionEmbedding = async (
+  question: string
+): Promise<number[]> => {
   //supabase small has a 512 token limit. Could consider chunking lengthy questions
   const embeddedQuestion = (
     await generateEmbedding(question, {
