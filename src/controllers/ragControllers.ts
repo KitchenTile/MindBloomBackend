42c98a0c9d45851159a0a0fde540b6d06d5bb827
Blue <azuldebenedetti@gmail.com>
2025-10-12 20:19:57 +0100

    Update ragControllers.ts

--- src/controllers/ragControllers.ts
+++ src/controllers/ragControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { handleUserQuery } from "../utils/userDataRetrival.js";
+import { handleUserQuery } from "../utils/queryRetrivalPipeline.js";
 import OpenAI from "openai";
 
 export const handleChat = async (req: Request, res: Response) => {
@@ -8,29 +8,46 @@ export const handleChat = async (req: Request, res: Response) => {
     const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
     const userQuery = req.body.userQuery;
 
+    if (!userQuery) {
+      return res.status(400).json({ message: "Query is required" });
+    }
+
     //similarity search
     const relataedChunks = await handleUserQuery(userQuery);
 
-    const chunkContent = relataedChunks.map((chunk) => chunk.chunk_content);
+    //Format the information returned in a way it's easily understood by the LLM, providing some metadata
+    const chunkContentFormatted = relataedChunks
+      .map(
+        (chunk: any, index: number) =>
+          `--- SOURCE ${index + 1} (Chapter ${chunk.chapter}) ---\n${chunk.chunk_content}\n`
+      )
+      .join("\n\n");
 
-    //shape the gpt promp
-    const gptReply = await client.chat.completions.create({
-      model: "gpt-4",
-      messages: [
-        {
-          role: "system",
-          content: `Please only use the following information: ${chunkContent}.`,
-        },
-        {
-          role: "user",
-          content: `${userQuery}`,
-        },
-      ],
-    });
+    console.log(chunkContentFormatted);
 
-    const replyContent = gptReply.choices[0].message.content;
+    // // shape the gpt promp
+    // const gptReply = await client.chat.completions.create({
+    //   model: "gpt-4",
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content: `You are a helpful assistant. Use the following documents to answer the user's question. If the answer is not contained in the documents, say "I don't have enough information to answer that question." Do not make up any information.
+    //       Documents:
+    //       ${chunkContentFormatted}`,
+    //     },
+    //     {
+    //       role: "user",
+    //       content: `${userQuery}`,
+    //     },
+    //   ],
+    //   max_tokens: 500,
+    // });
 
-    return res.status(201).json(replyContent);
+    // const replyContent = gptReply.choices[0].message.content;
+
+    // console.log(replyContent);
+
+    // return res.status(200).json(replyContent);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Server error");
