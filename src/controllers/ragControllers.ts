ba653820d3a555c94cc77022e315362ae3170f09
Blue <azuldebenedetti@gmail.com>
2025-10-16 14:39:03 +0100

    Update ragControllers.ts

--- src/controllers/ragControllers.ts
+++ src/controllers/ragControllers.ts
@@ -2,22 +2,38 @@ import { Request, Response } from "express";
 import { handleUserQuery } from "../utils/queryRetrivalPipeline.js";
 import OpenAI from "openai";
 import { supabase } from "../config/db.js";
+import { FunctionsFetchError } from "@supabase/supabase-js";
+import { title } from "process";
 
 export const handleChat = async (req: Request, res: Response) => {
   try {
     //init gpt client
     const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
     const userQuery = req.body.userQuery;
+    const chatId = req.body.chatId;
+    console.log("chatId");
+
+    console.log(chatId);
 
     if (!userQuery) {
       return res.status(400).json({ message: "Query is required" });
     }
 
+    //upload user message
+    await uploadMessage(chatId, userQuery, "user");
+    //get chat data
+    const chatMessages = await fetchChat(chatId);
+
+    // Format history for the LLM
+    const historyMessages = chatMessages
+      ? chatMessages.messages.map((msg: any) => ({
+          role: msg.role,
+          content: msg.content,
+        }))
+      : [];
+
     //similarity search
     const relataedChunks = await handleUserQuery(userQuery);
-
-    // console.log("CONTROLLER");
-    // console.log(relataedChunks);
 
     //Format the information returned in a way it's easily understood by the LLM, providing some metadata
     const chunkContentFormatted = relataedChunks
@@ -30,43 +46,98 @@ export const handleChat = async (req: Request, res: Response) => {
     console.log(chunkContentFormatted);
 
     console.log("CALL GPT REPLY");
-    // shape the gpt promp
-    const gptReply = await client.chat.completions.create({
-      model: "gpt-5-nano",
-      messages: [
-        {
-          role: "system",
-          content: `You are a helpful assistant. Use the following documents to answer the user's question, if you use differnet books for your answer, cite the book title, its author and the chapter of the book you used. If the answer is not contained in the documents, say "I don't have enough information to answer that question." Do not make up any information.
-          Documents:
-          ${chunkContentFormatted}`,
-        },
-        {
-          role: "user",
-          content: `${userQuery}`,
-        },
-      ],
-    });
+    // // shape the gpt promp
+    // const gptReply = await client.chat.completions.create({
+    //   model: "gpt-5-nano",
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content: `You are a helpful assistant. Use the following documents to answer the user's question, if you use differnet books for your answer, cite the book title, its author and the chapter of the book you used. If the answer is not contained in the documents, say "I don't have enough information to answer that question." Do not make up any information.
+    //       Documents:
+    //       ${chunkContentFormatted}`,
+    //     },
+    //      ...historyMessages
+    //     {
+    //       role: "user",
+    //       content: `${userQuery}`,
+    //     },
+    //   ],
+    // });
 
-    const replyContent = gptReply.choices[0].message.content;
+    // const replyContent = gptReply.choices[0].message.content;
 
-    console.log("reply");
-    console.log(replyContent);
+    // console.log("REPLY");
+    // console.log(replyContent);
 
-    return res.status(200).json(replyContent);
+    // await uploadMessage(chatId, replyContent, "assistant");
+
+    // return res.status(200).json(replyContent);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Server error");
   }
 };
 
-// const uploadMessage = async (chatId, message: string) => {
+export const getAllChats = async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase.from("chats").select();
 
-//   const messageObj =   { "role": "user", "content": message, "timestamp": Date.now() }
+    if (error) return error;
 
-//       const { error } = await supabase
-//         .from("chats")
-//         .update(message: message);
+    console.log("CHATS");
+    console.log(data);
+    return res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//       if (error) return error;
+const uploadMessage = async (chatId, message: string, role: string) => {
+  let newChat = true;
+  if (!chatId) {
+    console.log("No chat ID");
+  }
 
-//     }
+  //check if chatId exists in the database
+  const currentChat = await fetchChat(chatId);
+
+  if (currentChat.length !== 0) newChat = false;
+
+  // create timestamp, and form the new message object
+  const date = new Date(Date.now()).toISOString();
+  const messageObj = { role: role, content: message, timestamp: date };
+
+  if (newChat) {
+    const { error } = await supabase
+      .from("chats")
+      .insert({
+        chat_id: chatId,
+        title: chatId,
+        user_id: chatId,
+        messages: messageObj,
+        created_at: date,
+      });
+
+    console.log("UPLOAD NEW MESSAGE");
+    if (error) return error;
+  } else {
+    const { error } = await supabase.rpc("add_chat_message", {
+      p_chat_id: chatId,
+      p_new_message: messageObj,
+    });
+
+    console.log("UPDATE MESSAGFE ARRAY");
+    if (error) return error;
+  }
+};
+
+const fetchChat = async (chatId) => {
+  const { data, error } = await supabase
+    .from("chats")
+    .select("messages")
+    .eq("chat_id", chatId);
+
+  if (error) return error;
+
+  return data;
+};
