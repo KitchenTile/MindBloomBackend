c01160e12f52dc774a8a1ff7172e5fae01b0dfcd
Blue <azuldebenedetti@gmail.com>
2025-10-17 19:26:00 +0100

    Update ragRoutes.ts

--- src/routes/ragRoutes.ts
+++ src/routes/ragRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { getAllChats, handleChat } from "../controllers/ragControllers.js";
+import {
+  deleteChat,
+  editChatTitle,
+  getAllChats,
+  handleChat,
+} from "../controllers/ragControllers.js";
 
 const ragRouter = express.Router();
 
 //use the bussiness logic from the controllers
 ragRouter.post("/", handleChat);
 ragRouter.get("/", getAllChats);
+ragRouter.delete("/", deleteChat);
+ragRouter.put("/", editChatTitle);
 
 export default ragRouter;
