c052c8d4648163852b8e14f197bdbeef75f862e8
Blue <azuldebenedetti@gmail.com>
2025-10-23 22:13:00 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,7 @@ import { logger } from "./middleware/logger.js";
 import lessonRouter from "./routes/lessonRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 import ragRouter from "./routes/ragRoutes.js";
-import {
-  bookHandler,
-  recursiveTextChunkSplitter,
-} from "./utils/dataIngestionPipeline.js";
-// import { getQuestionEmbedding } from "./utils/userDataRetrival.js";
+import { bookHandler } from "./utils/dataIngestionPipeline.js";
 import fs from "fs";
 
 const app = express();
@@ -38,31 +34,11 @@ app.use("/lessons", lessonRouter);
 app.use("/orders", orderRouter);
 app.use("/chat", ragRouter);
 
-const bookMetadata = {
-  topic: "Full Stack Development Module",
-  title: "Fll Stack Handbook",
-  chapters: 10,
-  author: "Middlesex University",
-  year: 2025,
-};
-
-// const textExtract =
-//   "## Use R! _Series Editors:_ Robert Gentleman Kurt Hornik Giovanni G. Parmigiani For further volumes: [http://www.springer.com/series/6991](http://www.springer.com/series/6991) ## Kenneth Knoblauch \n • Laurence T. Maloney ## Modeling Psychophysical ## Data in R # 123 Kenneth Knoblauch Department of Integrative Neurosciences Stem-cell and Brain Research Institute INSERM U846 18 avenue du Doyen Lépine Bron, France Laurence T. Maloney Department of Psychology Center for Neural Science New York University 6 Washington Place, 2nd /n Floor New York, USA _Series Editors:_ Robert Gentleman Program in Computational Biology Division of Public Health Sciences Fred Hutchinson Cancer Research Center 1100 Fairview Ave. N, M2-B876 Seattle, Washington 98109-1024 USA \n Giovanni G. Parmigiani The Sidney Kimmel Comprehensive Cancer Center at Johns Hopkins University 550 North Broadway Baltimore, MD 21205-2011 USA";
-
-// getGptMetadata(textExtract);
-bookHandler("src/data/shortertext.md");
-
-// const data = await fs.promises.readFile("src/data/shortertext.md", {
-//   encoding: "utf-8",
-// });
-
-// console.log(data.split("\n"));
+// bookHandler("src/data/shortertext.md");
 
 app.listen(PORT, () => {
   console.log(`server running on localhost:${PORT}`);
 });
-
-// console.log(recursiveTextChunkSplitter(textExtract, 1000));
 
 app.use(
   (
