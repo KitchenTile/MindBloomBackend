65fbd03fea6605b6a217dc9d87e26a0892da7f10
Blue <azuldebenedetti@gmail.com>
2025-10-24 16:08:50 +0100

    Update passwordUtils.js

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -1,17 +1,3 @@
-import bcrypt from "bcrypt";
-
-export const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  console.log(hashedPassword);
-
-  return hashedPassword;
-};
-
-export const comparePassword = async (password, userPassword) => {
-  return await bcrypt.compare(password, userPassword);
-};
-
 export const isPasswordValid = (password) => {
   /*
    * At least one lowercase letter ((?=.*[a-z])).
