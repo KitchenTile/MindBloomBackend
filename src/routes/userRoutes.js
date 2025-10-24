b72d2db165f1c9b712efd46df97d8f9d4e6d772f
Blue <azuldebenedetti@gmail.com>
2025-10-24 11:04:56 +0100

    Create userRoutes.js

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createUser, login } from "../controllers/userControllers";
+
+const userRouter = express.Router();
+
+//use the bussiness logic from the controllers
+userRouter.post("/", createUser);
+userRouter.post("/login", login);
+
+// userRouter.get("/", getAllChats);
+// userRouter.delete("/", deleteChat);
+// userRouter.put("/", editChatTitle);
+
+export default userRouter;
