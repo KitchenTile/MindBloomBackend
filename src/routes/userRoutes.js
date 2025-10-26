dc068774048994ad5d219dbcdb86e15b30fa4161
Blue <azuldebenedetti@gmail.com>
2025-10-26 19:00:49 +0000

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createUser, login } from "../controllers/userControllers.js";
+import { createUser, login, logout } from "../controllers/userControllers.js";
 
 const userRouter = express.Router();
 
 //use the bussiness logic from the controllers
 userRouter.post("/", createUser);
 userRouter.post("/login", login);
+userRouter.post("/logout", logout);
 
 // userRouter.get("/", getAllChats);
 // userRouter.delete("/", deleteChat);
